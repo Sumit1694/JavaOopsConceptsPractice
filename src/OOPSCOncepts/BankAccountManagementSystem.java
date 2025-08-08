582875a4d3c8b0c8d2314ab6da82b95a56b8d91b
Sumit1694 <Sumitrane198@gamil.com>
2025-08-08 23:07:14 +0530

    My problem1 commit

---
+++ src/OOPSCOncepts/BankAccountManagementSystem.java
@@ -0,0 +1,39 @@
+package OOPSCOncepts;
+
+import java.util.ArrayList;
+
+public class BankAccountManagementSystem {
+
+	public static void main(String[] args) {
+		
+		Bank bank=new Bank();
+		
+		Account account1 = new Account("Peter England", "C0011", 5000);
+		Account account2 = new Account("Katja Ruedi", "C0121", 4500);
+		Account account3 = new Account("Marcella gabriells", "C0222", 20000);
+		
+		bank.addAccount(account1);
+		bank.addAccount(account2);
+		bank.addAccount(account3);
+		
+		ArrayList<Account> accounts = bank.getAccount();
+		
+		for(Account account:accounts)
+		{
+			System.out.println(account.getAccountInfo());
+		}
+		
+		System.out.println("\nAfter depositing 1000 into account1");
+		
+		bank.depositMoney(account1, 1000);
+		System.out.println(account1.getAccountInfo());
+		
+		System.out.println("No transaction in account2");
+		
+		System.out.println("After withdrawing 5000 from account3");
+		
+		bank.withdrawMoney(account3, 5000);
+		System.out.println(account3.getAccountInfo());
+		
+	}
+}
